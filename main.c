5cadee2e17faaafaeec6ab544a20ded099438d5c
Gunjanchaudhari03 <gunjupc@gmail.com>
2024-02-26 14:21:26 +0000

    Updated main.c

--- main.c
+++ main.c
@@ -5,9 +5,15 @@ Initial Function()
   //Sample Code
 }
 
+<<<<<<< .merge_file_tz0pyr
 Initial Security()
 {
  //Sample Code
+=======
+feature1()
+{
+  //Sample Code for Feature 1
+>>>>>>> .merge_file_gScTX2
 }
 
 
